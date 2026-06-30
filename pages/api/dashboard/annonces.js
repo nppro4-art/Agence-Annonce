06d7ea710e1c5dc226a4e2be9b932c482fd168ee
Ahno <np.pro4@gmail.com>
2026-06-30 07:19:53 +0200

    Delete pages directory

--- pages/api/dashboard/annonces.js
+++
@@ -1,8 +0,0 @@
-import { requireAuth } from '../../../lib/auth'
-import { prisma } from '../../../lib/db'
-export default requireAuth(async function(req, res) {
-  const annonces = await prisma.annonce.findMany({
-    where: { userId: req.user.id }, orderBy: { createdAt: 'desc' }, take: 20
-  })
-  res.status(200).json({ annonces })
-})
